b8dfdb3003333a19d038bef142d3dd261ff04e5c
zyd2001 <zyd200101@outlook.com>
2020-05-03 17:45:49 -0500

    auto format script

---
+++ format.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+v=`clang-format --version | grep -Po '\d+.\d+.\d+'`
+
+if [ `echo -e "$v\n10.0.0" | sort -V | head -n1` != '10.0.0' ]; then
+    echo "clang-format version too old"
+    exit;
+fi
+
+files=`ls src/* include/* test/*`
+
+for f in $files; do
+    `clang-format -style=file $f -i`
+done
